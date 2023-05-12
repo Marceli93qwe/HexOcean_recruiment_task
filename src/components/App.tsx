949027f177b98d7a959c5e1d4db6f04552570ef8
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-05-12 11:47:15 +0200

    prepared look of the order form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import {Box, Button, FormControl, InputLabel, MenuItem, Select, Stack, TextField} from "@mui/material";
 
 function App() {
     return (
@@ -11,6 +11,31 @@ function App() {
             width="50vw"
             height="100vh" // full window height
         >
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                console.log("ok")
+            }} action="/d">
+                <Stack spacing={5} useFlexGap flexWrap={"wrap"} sx={{
+                    textAlign: "center"
+                }}>
+                    <h2>Order your food</h2>
+                    <TextField id="outlined-basic" label="Dish name" variant="outlined" required={true}/>
+                    <TextField id="outlined-basic" label="Preparation time" variant="outlined"/>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Age"
+                        >
+                            <MenuItem>Ten</MenuItem>
+                            <MenuItem>Twenty</MenuItem>
+                            <MenuItem>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button type={"submit"} variant={"contained"}>Order</Button>
+                </Stack>
+            </form>
         </Box>
     );
 }
