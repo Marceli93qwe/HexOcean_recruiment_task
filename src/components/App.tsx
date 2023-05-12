f0c1bbe09beb40523b32ab4fecd0b559a856d967
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-05-12 11:25:09 +0200

    basic directory structure

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+function App() {
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            margin="auto"
+            width="50vw"
+            height="100vh" // full window height
+        >
+        </Box>
+    );
+}
+
+export default App;
